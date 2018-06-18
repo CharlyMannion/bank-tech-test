018b6c1376bc4ff318f8e21b6e10eb34f5d76fa1
Charly Mannion <charlottemannion100@gmail.com>
2018-06-18 15:38:22 +0100

    Refactor tests to include a before statement for a deposit to the bank account

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,29 +1,28 @@
 require './lib/bank_account'
 
 describe BankAccount do
+  subject(:account) {described_class.new}
 
   describe '#initialize' do
     it 'should have a default balance of nil' do
-      account = BankAccount.new
       expect(account.balance).to eq BankAccount::DEFAULT_BALANCE
     end
   end
 
-  describe '#deposit' do
-    it 'should increase the bank balance by the specified amount' do
-      account = BankAccount.new
+  describe 'tests that require a despoit of 1000 to have been made' do
+    before do
       account.deposit("10-01-2012", 1000)
-      expect(account.balance).to eq 1000
     end
-  end
-
-  describe '#withdraw' do
-    it 'should reduce the bank balance by the specified amount' do
-      account = BankAccount.new
-      account.deposit("10-01-2012", 1000)
-      account.deposit("13-01-2012", 2000)
-      account.withdraw("14-01-2012", 500)
-      expect(account.balance).to eq 2500
+    describe '#deposit' do
+      it 'should increase the bank balance by the specified amount' do
+        expect(account.balance).to eq 1000
+      end
+    end
+    describe '#withdraw' do
+      it 'should reduce the bank balance by the specified amount' do
+        account.withdraw("14-01-2012", 500)
+        expect(account.balance).to eq 500
+      end
     end
   end
 
