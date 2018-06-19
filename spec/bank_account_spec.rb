adf6ba4f9b71a964394bab84d40ef8d4c89a2808
Charly Mannion <charlottemannion100@gmail.com>
2018-06-19 15:51:43 +0100

    Test that making a withdrawal is recorded in the bank statement

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -36,13 +36,15 @@ describe BankAccount do
   describe '#withdraw' do
     before do
       account.withdraw('14-01-2012', 500)
-      p account.balance
     end
     it 'should reduce the bank balance by the specified amount' do
       expect(account.balance).to eq -500
     end
     it 'should create a transaction' do
-      expect(fake_transaction_class).to have_received(:new).with('14-01-2012', 1000, -500)
+      expect(fake_transaction_class).to have_received(:new).with('14-01-2012', 500, -500)
+    end
+    it 'should get recorded in the bank statement' do
+      expect(account.bank_statement).to include(transaction)
     end
   end
 end
