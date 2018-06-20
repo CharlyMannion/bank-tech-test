6bbecced5fd2457fdc7766600e5d97b8acaf96c1
Charly Mannion <charlottemannion100@gmail.com>
2018-06-20 14:54:09 +0100

    Refactored to remove unused doubles

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -4,11 +4,11 @@ require './lib/printer'
 
 describe Printer do
   subject(:printer) { described_class.new }
-  let(:bank_statement) { double(:bank_statement) }
-  let(:transaction) { double(:transaction) }
-  let(:transaction1) { double(:transaction, date: "10-01-2012", amount: 1000, balance: 1000) }
-  let(:transaction2) { double(:transaction, date: "13-01-2012", amount: 2000, balance: 3000) }
-  let(:transaction3) { double(:transaction, date: "14-01-2012", amount: 500, balance: 2500) }
+  # let(:bank_statement) { double(:bank_statement) }
+  # let(:transaction) { double(:transaction) }
+  # let(:transaction1) { double(:transaction, date: "10-01-2012", amount: 1000, balance: 1000) }
+  # let(:transaction2) { double(:transaction, date: "13-01-2012", amount: 2000, balance: 3000) }
+  # let(:transaction3) { double(:transaction, date: "14-01-2012", amount: 500, balance: 2500) }
 
   # expected = "      date || credit || debit || balance
   # 14/01/2012 || || 500.00 || 2500.00
@@ -26,6 +26,8 @@ describe Printer do
   describe '#print_format' do
     it 'should print the column headings' do
       expected_heading = "date || credit || debit || balance\n"
+      expected_transaction = "10/01/2012"
+      bank_statement = [expected_transaction]
       expect { printer.print_format(bank_statement) }.to output(expected_heading).to_stdout
     end
     # it 'should print the date of each transaction in the bank_statement' do
