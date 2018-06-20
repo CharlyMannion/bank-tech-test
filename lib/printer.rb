d64e17f991e17e3713c11d562d61ca6cdb8d93e3
Charly Mannion <charlottemannion100@gmail.com>
2018-06-20 15:46:42 +0100

    Refactor the printer class to use single quotes

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class Printer
-
   def print_format(bank_statement)
     print_heading
-    bank_statement.each { |transaction| puts transaction.date + " || " + transaction.amount.to_s + " || " + transaction.balance.to_s}
+    bank_statement.each { |transaction| puts transaction.date + ' || ' + transaction.amount.to_s + ' || ' + transaction.balance.to_s }
   end
 
   private
+
   def print_heading
-    puts "date || credit || debit || balance"
+    puts 'date || credit || debit || balance'
   end
 end
