66c2a63dde78af158868053736d87636a5f2bb35
Charly Mannion <charlottemannion100@gmail.com>
2018-06-20 10:30:15 +0100

    Refactored Print class test to test that an array of doubles is returned

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -5,14 +5,21 @@ require './lib/printer'
 describe Printer do
   subject(:printer) { described_class.new }
   let(:bank_statement) { double(:bank_statement) }
+  let(:transaction1) { double(:transaction) }
+  let(:transaction2) { double(:transaction) }
+  let(:transaction3) { double(:transaction) }
 
   it 'should respond to print' do
     expect(printer).to respond_to(:print)
   end
 
   describe '#print' do
-    it 'should print a bank statement' do
-      expect(printer.print(bank_statement)).to eq(bank_statement)
+    # it 'should print a bank statement' do
+    #   expect(printer.print(bank_statement)).to eq(bank_statement)
+    # end
+    it 'should print each transaction in the bank_statements' do
+      bank_statement_array = [transaction1, transaction2, transaction3]
+      expect(printer.print(bank_statement_array)).to eq([transaction1, transaction2, transaction3])
     end
   end
 end
