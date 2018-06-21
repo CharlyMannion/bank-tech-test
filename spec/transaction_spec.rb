6c77db7e7fbb28a72140ac430da3843b8414d021
Charly Mannion <charlottemannion100@gmail.com>
2018-06-21 10:46:13 +0100

    Tests that the transaction has a credit value

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -3,7 +3,7 @@
 require './lib/transaction'
 
 describe Transaction do
-  subject(:transaction) { described_class.new('10-01-2012', 1000, 1000) }
+  subject(:transaction) { described_class.new('10-01-2012', 0, 1000, 1000) }
 
   describe '#initialize' do
     it 'should have a date' do
@@ -11,6 +11,9 @@ describe Transaction do
     end
     it 'should have a value for debit' do
       expect(transaction.debit).to eq(1000)
+    end
+    it 'should have a value for credit' do
+      expect(transaction.credit).to eq(0)
     end
     it 'should have a balance' do
       expect(transaction.balance).to eq(1000)
